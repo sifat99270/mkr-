d9c9c4cfacdd5389a453790f24e6cb8c1ba1c441
sifat <rasifat33@gmail.com>
2024-03-14 21:14:11 +0600

    king

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config, options) => {
-        if (!options.dev) {
-          config.devtool = options.isServer ? true : 'your-custom-devtool'
-        }
-        return config
-      },
-      async headers() {
-        return [
-          {
-            // matching all API routes
-            source: "/api/:path*",
-            headers: [
-              { key: "Access-Control-Allow-Credentials", value: "true" },
-              { key: "Access-Control-Allow-Origin", value: "*" },
-              { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-              { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-            ]
-          }
-        ]
-      }
+ 
+   
 };
 
 export default nextConfig;
